ba3e7c11a4e74ce3739da6dbfa2e98555f0c7a22
kvitebjorn <harintonkairu@gmail.com>
2023-11-10 13:56:11 -0700

    faster bounded prime generator

--- README.md
+++ README.md
@@ -2,3 +2,8 @@
 to run a problem, for example problem 1, assuming ghc is installed:
 
 `ghc -o problem1 problem1.hs && ./problem1`
+
+
+to generate performance metrics:
+
+`ghc -prof -fprof-auto -rtsopts -o problem1 problem1.hs && ./problem1 +RTS -p`
--- problem7.hs
+++ problem7.hs
@@ -1,7 +1,19 @@
 module Main where
 
-primes :: [Int]
-primes = [n | n <- [2..], all ((> 0) . rem n) [2..n-1]]
+minus :: Ord a => [a] -> [a] -> [a]
+minus (x:xs) (y:ys) = case compare x y of
+           LT -> x : minus  xs  (y:ys)
+           EQ ->     minus  xs     ys
+           GT ->     minus (x:xs)  ys
+minus  xs     _     = xs
+
+-- https://wiki.haskell.org/Prime_numbers
+primesToG :: (Ord a, Num a, Enum a) => a -> [a]
+primesToG m = 2 : sieve [3,5..m]
+    where
+    sieve (p:xs) 
+       | p*p > m   = p : xs
+       | otherwise = p : sieve (xs `minus` [p*p, p*p+2*p..])
 
 main :: IO ()
-main = print $ last $ take 10001 primes
+main = print $ last $ take 10001 $ primesToG 2000000
