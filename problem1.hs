aab78972fad2702665037406ba9523372f2d7490
kvitebjorn <harintonkairu@gmail.com>
2023-11-09 21:13:59 -0700

    add problem 1 problem statement

--- problem1.hs
+++ problem1.hs
@@ -1,5 +1,5 @@
 module Main where
 
--- ghc -o problem1 problem1.hs && ./problem1
+-- Problem 1. Find the sum of all the multiples of 3 or 5 below 1000
 main :: IO ()
 main = print (show (sum [i | i <- [3..999], mod i 3 == 0 || mod i 5 == 0]))
