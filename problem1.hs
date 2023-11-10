58d7f8c5589a88dae815880f506c9383463ca704
kvitebjorn <harintonkairu@gmail.com>
2023-11-10 00:06:24 -0700

    clean up problem 1 quick

--- problem1.hs
+++ problem1.hs
@@ -1,5 +1,4 @@
 module Main where
 
--- Problem 1. Find the sum of all the multiples of 3 or 5 below 1000
 main :: IO ()
-main = print (show (sum [i | i <- [3..999], mod i 3 == 0 || mod i 5 == 0]))
+main = print $ sum [i | i <- [3..999], mod i 3 == 0 || mod i 5 == 0]
