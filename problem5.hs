9cbfa5052ba18c192af8b1ccef44bb482a2453a5
kvitebjorn <harintonkairu@gmail.com>
2023-11-10 12:55:57 -0700

    slight improvement problem 5

--- problem5.hs
+++ problem5.hs
@@ -1,4 +1,4 @@
 module Main where
 
 main :: IO ()
-main = print $ head $ take 1 [n | n <- [2520, 2540..], all ((==0) . mod n) [1..20] ]
+main = print $ head $ take 1 [n | n <- [2520, 2540..], all ((==0) . mod n) [11..20] ]
